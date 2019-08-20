c437c3276ca291488b547c909b3b91894565a927
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-08-20 15:17:06 -0400

    more jwttoken with help from tutorial

---
+++ src/main/java/com/example/Server/Security/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.Server.Security;
+
+public class WebSecurityConfig {
+}
--- src/main/java/com/example/Server/Services/UserService.java
+++ src/main/java/com/example/Server/Services/UserService.java
@@ -1,5 +1,28 @@
 package com.example.Server.Services;
 
+
+import com.example.Server.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public String signin(String username, String password){
+    }
 
 }
