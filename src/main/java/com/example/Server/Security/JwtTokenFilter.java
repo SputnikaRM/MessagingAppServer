3736110df8bc25608133951f7113d087b9bb3e2f
Sputnika <Sputnika@SPUTs-MacBook-Pro.local>
2019-08-23 14:01:25 -0400

    update

--- src/main/java/com/example/Server/Security/JwtTokenFilter.java
+++ src/main/java/com/example/Server/Security/JwtTokenFilter.java
@@ -1,4 +1,62 @@
 package com.example.Server.Security;
 
-public class JwtTokenFilter {
+import com.example.Server.exception.CustomException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(httpServletRequest);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (CustomException ex) {
+            SecurityContextHolder.clearContext();
+            httpServletResponse.sendError(ex.getHttpStatus().value(), ex.getMessage());
+            return;
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
